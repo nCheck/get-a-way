f38879d0987efee50b4c615ba32d0b1c6fff118a
Cajetan <rigrod1999@gmail.com>
2018-10-05 12:09:39 +0530

    Initial Template

---
+++ client/sample/App/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, Component } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import {RouterModule} from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+const appRoutes = [
+  {path : '', component : HomeComponent},
+  {path : 'about', component : AboutComponent},
+  {path : 'login' , component : LoginComponent},
+  {path : 'signup' , component : SignupComponent},
+  {path : 'header' , component : HeaderComponent},
+  {path : 'footer', component : FooterComponent}
+];
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SignupComponent,
+    LoginComponent,
+    HomeComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
