4ee5ff29da032ea436c544143e57ed791997c319
Ashley Lobo <ashleyl@rediffmail.com>
2018-10-06 02:37:12 +0530

    Auto stash before merge of "master" and "origin/master"

--- client/YourFront/TourFront/src/app/maps/maps.component.ts
+++ client/YourFront/TourFront/src/app/maps/maps.component.ts
@@ -1,16 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AgmCoreModule } from '@agm/core';
 
 
 
 @Component({
   selector: 'app-maps',
-  template: `
-  <agm-map [latitude]="lat" [longitude]="lng"></agm-map>
-  `,
-  styleUrls: ['./maps.component.css']
+  templateUrl: './maps.component.html',
+  styleUrls: ['./maps.component.css'],
+
 })
 export class MapsComponent implements OnInit {
 
